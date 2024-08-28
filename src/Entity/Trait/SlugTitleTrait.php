97531fa14a8c69fb7f6e0d73831d37082b2dc2e3
Albert Arques <albertarques@gmail.com>
2024-08-28 11:13:47 +0200

    Add SlugTitleTrait.php

---
+++ src/Entity/Trait/SlugTitleTrait.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Entity\Trait;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+trait SlugTitleTrait
+{
+    #[Gedmo\Slug(fields: ['title'])]
+    #[ORM\Column(type: Types::STRING, length: 255, nullable: false)]
+    private string $slug;
+
+    public function getSlug(): string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+}
